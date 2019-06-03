1d77d12f749f7b0afeb1eaa3a8c96aed52b588b6
Alexey <redspirit2@ya.ru>
2019-06-03 17:59:23 +0300

    bullet fire

--- scripts/Bullet.gd
+++ scripts/Bullet.gd
@@ -1,26 +1,40 @@
 extends KinematicBody2D
 
 
-var SPEED = 4;
+var SPEED = 150;
 var dir = ''
 var isAction = false
+var motion = Vector2()
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	if isAction :
-		if dir == 'up' :
-			position.y -= SPEED
-		if dir == 'down' :
-			position.y += SPEED
-		if dir == 'left' :
-			position.x -= SPEED
-		if dir == 'right' :
-			position.x += SPEED
+		position += motion * SPEED * delta
+		
 
 
-func start(_dir) :
+func start(point, _dir) :
 	dir = _dir;
 	isAction = true;
-	print('START');
+	position = point
+	
+	if dir == 'up' :
+		position.y -= 20
+		motion = Vector2(0, -1);
+		$Sprite.frame = 0
+	if dir == 'down' :
+		position.y += 20
+		motion = Vector2(0, 1);
+		$Sprite.frame = 6
+	if dir == 'left' :
+		position.x -= 20
+		motion = Vector2(-1, 0);
+		$Sprite.frame = 1
+	if dir == 'right' :
+		position.x += 20
+		motion = Vector2(1, 0);
+		$Sprite.frame = 7
+	
+	print('START ', dir);
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -2,8 +2,12 @@ extends KinematicBody2D
 
 
 var SPEED = 80;
+var dir = "";
 
 var Bullet = preload("res://scenes/Bullet.tscn");
+
+func _ready():
+	dir = "up"
 
 func _physics_process(delta):
 
@@ -12,19 +16,25 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		motion = Vector2(0, -1);
 		$anim.current_animation = "up"
+		dir = "up"
 	elif Input.is_action_pressed("ui_down"):
 		motion = Vector2(0, 1);
 		$anim.current_animation = "down"
+		dir = "down"
 	elif Input.is_action_pressed("ui_left"):
 		motion = Vector2(-1, 0);
 		$anim.current_animation = "left"
+		dir = "left"
 	elif Input.is_action_pressed("ui_right"):
 		motion = Vector2(1, 0);
 		$anim.current_animation = "right"
+		dir = "right"
 	else :
 		$anim.current_animation = ""
 		
 	if Input.is_action_just_pressed("ui_select") :
-		print("PRESSED")
+		var bul = Bullet.instance()
+		bul.start(position, dir)
+		$"../".add_child(bul)
 		
 	move_and_slide(motion * SPEED);
